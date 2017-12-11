efc83b173d7b00e108ed0c1aa0736d0a6ac633d5
Ricardo Ledo de Tulio <ledo.tulio@gmail.com>
2017-12-11 20:48:22 -0200

    login: create a simple form to login into application

--- public/index.php
+++ public/index.php
@@ -1,1 +1,19 @@
-<?php echo "Hello, world!";
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App;
+$container = $app->getContainer();
+
+$container['view'] = new \Slim\Views\PhpRenderer("../src/views/");
+
+$app->get('/', function (Request $request, Response $response) {
+    $response = $this->view->render($response, "login/login.phtml");
+
+    return $response;
+});
+
+$app->run();
