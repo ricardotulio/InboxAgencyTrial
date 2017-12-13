875b32b1294f48e9d9aa1195c70d0f3a6b048615
Ricardo Ledo de Tulio <ledo.tulio@gmail.com>
2017-12-13 13:03:05 -0200

    chore: improve code coverage implementing CartTest

--- test/Cart/Service/CartTest.php
+++ test/Cart/Service/CartTest.php
@@ -4,6 +4,8 @@ namespace InboxAgency\Cart\Service;
 
 use PHPUnit\Framework\TestCase;
 use InboxAgency\Session\Session;
+use InboxAgency\Cart\Entity\Cart as CartEntity;
+use inboxAgency\Catalog\Entity\Product;
 
 class CartTest extends TestCase
 {
@@ -34,5 +36,102 @@ class CartTest extends TestCase
             ->willReturn($cart);
 
         $this->assertSame($cart, $service->getCart());
+    }
+
+    /**
+     * @test
+     */
+    public function mustAddProductToCart()
+    {
+        $session = $this->createMock(Session::class);
+        $cart = $this->createMock(CartEntity::class);
+        $product = $this->createMock(Product::class);
+
+        $session->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('cart'))
+            ->willReturn($cart);
+
+        $session->expects($this->once())
+            ->method('set')
+            ->with(
+                $this->equalTo('cart'),
+                $this->equalTo($cart)
+            );
+
+        $cart->expects($this->once())
+            ->method('addCartItem');
+
+        $product->method('getId')
+            ->willReturn(10);
+
+        $service = new Cart($session);
+        $service->addProduct($product);
+    }
+
+
+    /**
+     * @test
+     */
+    public function mustVerifyIfHasItems()
+    {
+        $session = $this->createMock(Session::class);
+        $cart = $this->createMock(CartEntity::class);
+
+        $cart->expects($this->once())
+            ->method('hasItems')
+            ->willReturn(true);
+
+        $session->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('cart'))
+            ->willReturn($cart);
+
+        $service = new Cart($session);
+
+        $this->assertTrue($service->hasItems());
+    }
+
+    /**
+     * @test
+     */
+    public function mustRemoveItem()
+    {
+        $itemId = 10;
+
+        $session = $this->createMock(Session::class);
+        $cart = $this->createMock(CartEntity::class);
+
+        $cart->expects($this->once())
+            ->method('removeCartItem')
+            ->with($this->equalTo($itemId));
+
+        $session->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('cart'))
+            ->willReturn($cart);
+
+        $service = new Cart($session);
+        $service->removeProduct($itemId);
+    }
+
+    /**
+     * @test
+     */
+    public function mustCleanCart()
+    {
+        $session = $this->createMock(Session::class);
+        $cart = $this->createMock(CartEntity::class);
+
+        $cart->expects($this->once())
+            ->method('cleanCart');
+
+        $session->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo('cart'))
+            ->willReturn($cart);
+
+        $service = new Cart($session);
+        $service->cleanCart();
     }
 }
